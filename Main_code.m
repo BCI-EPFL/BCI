d85709315c5429ea9b94f52ddef11b9a8f52182d
Elisabetta <pagliara.elisabetta@gmail.com>
2018-04-14 11:44:22 +0200

    mod2

--- Main_code.m
+++ Main_code.m
@@ -23,6 +23,7 @@ session.Event_pos=h.EVENT.POS;
 %% pwelch on raw data
 epoch_baseline=epoch_struct(session,200,0,3);
 epoch_MI=epoch_struct(session,400,-2,6);
+epoch_MI_termination=epoch_struct(session,555,-3,3);
 
 for i=1:size(chanlocs16,2)
    [pwelch_bas_onechannel{i},freq_1]=pwelch_for_each_channel(i,epoch_baseline,500,epoch_baseline.fs); 
@@ -48,6 +49,7 @@ session_filt=session;
 session_filt.data=data_filter;
 filt_epoch_baseline=epoch_struct(session_filt,200,0,3);
 filt_epoch_MI=epoch_struct(session_filt,400,0,3);
+filt_epoch_MI_termination=epoch_struct(session_filt,555,-3,3);
 
 for i=1:size(chanlocs16,2)
    [filt_pwelch_bas_onechannel,freq_1]=pwelch_for_each_channel(i,filt_epoch_baseline,500,filt_epoch_baseline.fs); 
@@ -83,6 +85,7 @@ session_filt_CAR.data=signal_car';
 %%p_welch on car data
 filt_epoch_baseline_CAR=epoch_struct(session_filt_CAR,200,0,3);
 filt_epoch_MI_CAR=epoch_struct(session_filt_CAR,400,-2,6);
+filt_epoch_MI_CAR_termination=epoch_struct(session_filt_CAR,555,-3,3);
 
 for i=1:size(chanlocs16,2)
    [pwelch_car_bas_onechannel{i},freq_1]=pwelch_for_each_channel(i,filt_epoch_baseline_CAR,500,session_filt_CAR.fs); 
@@ -128,63 +131,79 @@ signal_laplacian = s(:,1:16)*lap;
 % subplot(3,1,3)
 % plot(signal_car(:,9))
 
+
 session_filt_lap=session;
 session_filt_lap.data=signal_laplacian';
 filt_epoch_baseline_lap=epoch_struct(session_filt_lap,200,0,3);
 filt_epoch_MI_lap=epoch_struct(session_filt_lap,400,-2,6);
-
-%% spectrogram
+filt_epoch_MI_lap_termination=epoch_struct(session_filt_lap,555,-3,3);
+%% spectrogram_motor_imagenery_initiation
 
 Cyclic_freq=[5:0.1:40];
 
 for i=1:16
     
    [spect_for_one_channel,t, f]=Spectrogram_function(epoch_baseline, epoch_MI, i, epoch_baseline.fs, epoch_baseline.fs-32, Cyclic_freq);
-   
-  % spect_tot(i,:,:)=spect_for_one_channel;
-   
-   figure;
-   imagesc('XData',t,'YData',f,'CData', 10*log10(spect_for_one_channel)); % in order to put in line the ferquencies and teh time
-    %caxis([-5 5]);
+   subplot(4,4,i)
+   imagesc('XData',t,'YData',f, 'CData',10*log10(spect_for_one_channel)); 
+   axis tight
    xlabel('time[s]');
    ylabel('frequency[Hz]');
-   title((sprintf('Spectogram on raw signal for the channel %s',epoch_baseline.channels{1,i})));
+   h=colorbar;
+   set(h,'ylim',[-5 5]);
+   title((sprintf('Spectogram raw for the channel %s for MI initiation',epoch_baseline.channels{1,i})));
+end
+
+%% spectrogram_motor_imagenery_termination
+Cyclic_freq=[5:0.1:40];
+
+for i=1:16
+    
+   [spect_for_one_channel,t, f]=Spectrogram_function(epoch_baseline, epoch_MI_termination, i, epoch_baseline.fs, epoch_baseline.fs-32, Cyclic_freq);
+   subplot(4,4,i)
+   imagesc('XData',t,'YData',f, 'CData',10*log10(spect_for_one_channel)); 
+   axis tight
+   xlabel('time[s]');
+   ylabel('frequency[Hz]');
+   h=colorbar;
+   set(h,'ylim',[-5 5]);
+   title((sprintf('Spectogram raw for the channel %s for MI termination',epoch_baseline.channels{1,i})));
 end
 
 
-%% spectogram_CAR
+
+%% spectogram_CAR_motor_imagery_initiation
 for i=1:16
     
    [spect_for_one_channel,t, f]=Spectrogram_function(filt_epoch_baseline_CAR, filt_epoch_MI_CAR, i, filt_epoch_baseline_CAR.fs, filt_epoch_baseline_CAR.fs-32, Cyclic_freq);
-   
-  % spect_tot(i,:,:)=spect_for_one_channel;
-   
    figure;
    imagesc('XData',t,'YData',f,'CData', 10*log10(spect_for_one_channel)); % in order to put in line the ferquencies and teh time
-    caxis([-5 5])
+   caxis([-5 5]);
    xlabel('time[s]');
    ylabel('frequency[Hz]');
    title((sprintf('Spectrogram on CAR signal for the channel %s',epoch_baseline.channels{1,i})));
    
 end
+%% spectogram_CAR_motor_imagery_termination
 
-%% spectrogram_Laplacian 
+
+%% spectrogram_Laplacian_motor_imagery_initiation
 for i=1:16
     
    [spect_for_one_channel,t, f]=Spectrogram_function(filt_epoch_baseline_lap, filt_epoch_MI_lap, i, filt_epoch_baseline_lap.fs, filt_epoch_baseline_lap.fs-32, Cyclic_freq);
-   
-  % spect_tot(i,:,:)=spect_for_one_channel;
-   
    figure;
    imagesc('XData',t,'YData',f,'CData', 10*log10(spect_for_one_channel)); % in order to put in line the ferquencies and teh time
-    caxis([-5 5])
+   caxis([-5 5]);
    xlabel('time[s]');
    ylabel('frequency[Hz]');
    title((sprintf('Spectrogram on Laplacian signal for the channel %s',epoch_baseline.channels{1,i})));
 end
-%% topoplot_raw data
+%% spectrogram_Laplacian_motor_imagery_termination
+
+
+%% topoplot_raw data_fre:8-12;
 spect_for_one_channel_top=zeros(16,1);
-Cyclic_freq2=[13:0.1:25];
+Cyclic_freq2=[8:0.1:12];
 for i=1:16
   
     [spect_for_one_channel,t, f]=Spectrogram_function(epoch_baseline, epoch_MI, i, epoch_baseline.fs, epoch_baseline.fs-32, Cyclic_freq2);
@@ -196,8 +215,9 @@ end
     
     topoplot(spect_for_one_channel_top,chanlocs16,'style','both','electrodes','ptslabels','chaninfo', session.channels);
 
+%% topoplot_raw data_fre:13-25;
 
-%% topoplot_car
+%% topoplot_car_fre:8-12;
 
 spect_for_one_channel_top=zeros(16,1);
 
@@ -214,8 +234,11 @@ end
 
 
 
+%% topoplot_car_fre:13-25;
 
-%% topoplot laplacian
+
+
+%% topoplot laplacian_fre:8-12;
 spect_for_one_channel_top=zeros(16,1);
 
 for i=1:16
@@ -230,4 +253,4 @@ end
     topoplot(spect_for_one_channel_top,chanlocs16,'style','both','electrodes','ptslabels','chaninfo', session.channels);
 
 
-
+%% topoplot laplacian_fre:13-25
--- epoch_struct.m
+++ epoch_struct.m
@@ -6,16 +6,15 @@
  struct_epoch.channels=session.channels;
  struct_epoch.Event_type=session.Event_type(session.Event_type==Align_Event);
  struct_epoch.Event_pos=session.Event_pos(session.Event_type==Align_Event);
- NumTrials=size(struct_epoch.Event_type,1); % because in each trials we'll have the same epoch, considering one is still enough.
+ NumTrials=size(struct_epoch.Event_type,1); 
  NumChannels=size(s,2);
  cstart=abs((timebEvent*struct_epoch.fs)-struct_epoch.Event_pos-1);%position of the aligned event minus the afterEvent
  cstop=(timeaEvent*struct_epoch.fs)+struct_epoch.Event_pos-1;
  
  for trId=1:NumTrials
-    cstart_=cstart(trId);%position of the aligned event minus the afterEvent
+    cstart_=cstart(trId)
     cstop_=cstop(trId);
-    %disp(['continuos feedback for trial' ,num2str( trId), 'start at', num2str( cstart) ,'end at', num2str(cstop)]);
-    ContFeed(:,:,trId)=s(cstart_:cstop_,1:NumChannels); %(time-sample, channels,trials)    %extract the continuos feed of the data from the file
+    ContFeed(:,:,trId)=s(cstart_:cstop_,1:NumChannels); 
 end
  struct_epoch.data=ContFeed;
   end
