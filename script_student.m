414b31e469102ebe843737da98543ecf8838316d
MilanoM <37181882+MilanoM@users.noreply.github.com>
2018-05-04 19:16:32 +0200

    Normalization + BaseMiTerm

--- script_student.m
+++ script_student.m
@@ -91,6 +91,9 @@ epoch_MI_term=epoch_window(runconc,555,0,3,params_spectrogram.mlength,params_spe
  
 thresholdCross=0.75; %25% of the data is test set
 
+%-----
+%BASELINE vs MI
+
 EpochTraining.BaseMI.data=cat(3, epoch_baseline.samples(:,:,1:thresholdCross*size(epoch_baseline.samples,3)), epoch_MI.samples(:,:,1:thresholdCross*size(epoch_MI.samples,3)));
 EpochTraining.BaseMI.labels=cat(1,epoch_baseline.labels(1:thresholdCross*size(epoch_baseline.labels,1)), epoch_MI.labels(1:thresholdCross*size(epoch_MI.labels,1)));
  
@@ -125,34 +128,112 @@ for i=1:10
    ValidationFold.BaseMI.data=Folds.BaseMI.data{i};
    ValidationFold.BaseMI.labels=Folds.BaseMI.labels{i};
    
+   %normalization
+   [TrainingFolds.BaseMI.data, mu.BaseMI, sigma.BaseMI]=zscore(TrainingFolds.BaseMI.data);
+   ValidationFold.BaseMI.data=(ValidationFold.BaseMI.data-mu.BaseMI)./sigma.BaseMI;
+   
    %Fisher's score: we need to save it for every feature for every
    %iteration (we will make the average outside the CV loop)
-   [ind(i,:), power_feat(i,:)] = rankfeat(TrainingFolds.BaseMI.data, TrainingFolds.BaseMI.labels,  'fisher');
+   [ind.BaseMI(i,:), power_feat.BaseMI(i,:)] = rankfeat(TrainingFolds.BaseMI.data, TrainingFolds.BaseMI.labels,  'fisher');
 
    %loop over the number of features
    for Nsel=[1:14,15:5:50,75:25:304]
-       classifier=fitcdiscr(TrainingFolds.BaseMI.data(:,ind(1:Nsel)),TrainingFolds.BaseMI.labels,'discrimtype','linear');
-       [yhat,PosteriorProb,~]=predict(classifier,ValidationFold.BaseMI.data(:,ind(1:Nsel)));
-       ClassError(i,Nsel)=classerror(ValidationFold.BaseMI.labels,yhat);
+       classifier.BaseMI=fitcdiscr(TrainingFolds.BaseMI.data(:,ind.BaseMI(1:Nsel)),TrainingFolds.BaseMI.labels,'discrimtype','linear');
+       [yhat.BaseMI,PosteriorProb.BaseMI,~]=predict(classifier.BaseMI,ValidationFold.BaseMI.data(:,ind.BaseMI(1:Nsel)));
+       ClassError.BaseMI(i,Nsel)=classerror(ValidationFold.BaseMI.labels,yhat.BaseMI);
        Nsel
    end
 end
 
-MeanClassError=mean(ClassError);
+MeanClassError.BaseMI=mean(ClassError.BaseMI);
 
 %average outside CV
-averageFisher=zeros(size(power_feat,2),1);
-for i=1:size(power_feat,2)
+averageFisher.BaseMI=zeros(size(power_feat.BaseMI,2),1);
+for i=1:size(power_feat.BaseMI,2)
     for j=1:10
-        averageFisher(i)=averageFisher(i)+power_feat(j,ind(j,:)==i);
+        averageFisher.BaseMI(i)=averageFisher.BaseMI(i)+power_feat.BaseMI(j,ind.BaseMI(j,:)==i);
     end
-    averageFisher(i)=averageFisher(i)/10;
+    averageFisher.BaseMI(i)=averageFisher.BaseMI(i)/10;
 end
 
 %reshaping average Fisher's scores from line to matrix to plot them
-averageFisher=reshape(averageFisher,[19 16])';
+averageFisher.BaseMI=reshape(averageFisher.BaseMI,[19 16])';
 
 %plot of Fisher's scores
 figure
-imagesc([4 40],[1 16],averageFisher)
+imagesc([4 40],[1 16],averageFisher.BaseMI)
 title('Fisher scores - baseline vs MI')
+
+
+%-----
+%BASELINE vs MI Termination
+
+EpochTraining.BaseMITerm.data=cat(3, epoch_baseline.samples(:,:,1:thresholdCross*size(epoch_baseline.samples,3)), epoch_MI_term.samples(:,:,1:thresholdCross*size(epoch_MI_term.samples,3)));
+EpochTraining.BaseMITerm.labels=cat(1,epoch_baseline.labels(1:thresholdCross*size(epoch_baseline.labels,1)), epoch_MI_term.labels(1:thresholdCross*size(epoch_MI_term.labels,1)));
+ 
+EpochTesting.BaseMITerm.data=cat(3, epoch_baseline.samples(:,:,(thresholdCross*size(epoch_baseline.samples,3)+1):end), epoch_MI_term.samples(:,:,(thresholdCross*size(epoch_MI_term.samples,3)+1):end));
+EpochTesting.BaseMITerm.labels=cat(1, epoch_baseline.labels((thresholdCross*size(epoch_baseline.samples,1)+1):end), epoch_MI_term.labels((thresholdCross*size(epoch_MI_term.samples,1)+1):end));
+
+NoTrainingSamplesPerClass=thresholdCross*size(epoch_baseline.samples,3);
+TrialsPerFold=NoTrainingSamplesPerClass/(33*10);  %33=no. of windows per trial, 10=folds cv;
+for i=1:10
+    contStart=(TrialsPerFold*(i-1)*33+1);
+    contEnd=TrialsPerFold*i*33;
+    Folds.BaseMITerm.data{i}=cat(3,EpochTraining.BaseMITerm.data(:,:,contStart:contEnd),EpochTraining.BaseMITerm.data(:,:,(contStart+NoTrainingSamplesPerClass):(contEnd+NoTrainingSamplesPerClass)));
+    Folds.BaseMITerm.labels{i}=cat(1,EpochTraining.BaseMITerm.labels(contStart:contEnd),EpochTraining.BaseMITerm.labels((contStart+NoTrainingSamplesPerClass):(contEnd+NoTrainingSamplesPerClass)));
+end
+
+%Now we need to transform every sample from a matrix to a line (because
+%rankfeat wants lines)
+a=Folds; 
+Folds.BaseMITerm=rmfield(Folds.BaseMITerm,'data');
+for i=1:10
+    for j=1:size(a.BaseMITerm.data{1,i},3)
+        Folds.BaseMITerm.data{1,i}(j,:)=reshape(a.BaseMITerm.data{1,i}(:,:,j)',[1,16*19]); 
+    end    
+end
+
+%---
+%Proper CV
+for i=1:10
+   %definition of the folds
+   TrainingFolds.BaseMITerm.data=cat(1,Folds.BaseMITerm.data{[1:(i-1),(i+1):10]});
+   TrainingFolds.BaseMITerm.labels=cat(1,Folds.BaseMITerm.labels{[1:(i-1),(i+1):10]});
+   ValidationFold.BaseMITerm.data=Folds.BaseMITerm.data{i};
+   ValidationFold.BaseMITerm.labels=Folds.BaseMITerm.labels{i};
+   
+   %normalization
+   [TrainingFolds.BaseMITerm.data, mu.BaseMITerm, sigma.BaseMITerm]=zscore(TrainingFolds.BaseMITerm.data);
+   ValidationFold.BaseMITerm.data=(ValidationFold.BaseMITerm.data-mu.BaseMITerm)./sigma.BaseMITerm;
+   
+   %Fisher's score: we need to save it for every feature for every
+   %iteration (we will make the average outside the CV loop)
+   [ind.BaseMITerm(i,:), power_feat.BaseMITerm(i,:)] = rankfeat(TrainingFolds.BaseMITerm.data, TrainingFolds.BaseMITerm.labels,  'fisher');
+
+   %loop over the number of features
+   for Nsel=[1:14,15:5:50,75:25:304]
+       classifier.BaseMITerm=fitcdiscr(TrainingFolds.BaseMITerm.data(:,ind.BaseMITerm(1:Nsel)),TrainingFolds.BaseMITerm.labels,'discrimtype','linear');
+       [yhat.BaseMITerm,PosteriorProb.BaseMITerm,~]=predict(classifier.BaseMITerm,ValidationFold.BaseMITerm.data(:,ind.BaseMITerm(1:Nsel)));
+       ClassError.BaseMITerm(i,Nsel)=classerror(ValidationFold.BaseMITerm.labels,yhat.BaseMITerm);
+       Nsel
+   end
+end
+
+MeanClassError.BaseMITerm=mean(ClassError.BaseMITerm);
+
+%average outside CV
+averageFisher.BaseMITerm=zeros(size(power_feat.BaseMITerm,2),1);
+for i=1:size(power_feat.BaseMITerm,2)
+    for j=1:10
+        averageFisher.BaseMITerm(i)=averageFisher.BaseMITerm(i)+power_feat.BaseMITerm(j,ind.BaseMITerm(j,:)==i);
+    end
+    averageFisher.BaseMITerm(i)=averageFisher.BaseMITerm(i)/10;
+end
+
+%reshaping average Fisher's scores from line to matrix to plot them
+averageFisher.BaseMITerm=reshape(averageFisher.BaseMITerm,[19 16])';
+
+%plot of Fisher's scores.
+figure
+imagesc([4 40],[1 16],averageFisher.BaseMITerm)
+title('Fisher scores - baseline vs MI termination')
