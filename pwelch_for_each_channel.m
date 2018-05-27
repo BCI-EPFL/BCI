d0d69088790f0335d252fa898863226c6130e722
GiammarcoP <giammarco.pacifico@epfl.ch>
2018-05-27 18:08:04 +0200

    pwelch

--- pwelch_for_each_channel.m
+++ pwelch_for_each_channel.m
@@ -1,7 +1,7 @@
 function [pow, freq]=pwelch_for_each_channel(num_channel,epoched_data,f,fs)
     pow=0;
     for i=1:size(epoched_data.data,3)
-        [a,b]=pwelch(squeeze(epoched_data.data(:,num_channel,i)),fs,0.5*0.5*fs,f,1);
+        [a,b]=pwelch(squeeze(epoched_data.data(:,num_channel,i)),fs,0.5*fs,f,512);
         pow=pow+a;
     end
     pow=pow/i;
